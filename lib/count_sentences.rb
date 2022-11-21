95e78f2b49d813a38389fa14a215c50be7e40642
Tim <timtran007@gmail.com>
2022-11-20 21:08:39 -0600

    commit tree 109e8a9d5123b0edbf54b73782a4438e59966511

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    words = self.split(" ")
+    array_of_truthy = words.map do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end
+    array_of_truthy.count(true)
+    end
 end
